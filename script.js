fea05ca019dc88ef77ba76ba42c6fd55bac16d44
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2023-04-17 22:30:49 +0200

    add stop

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-console.log("salut ca va")
-
-// Check if browser supports Web Speech API
 if ('webkitSpeechRecognition' in window) {
-	// Create new instance of SpeechRecognition
+
 	let recognition = new webkitSpeechRecognition();
 
-	// Set recognition parameters
 	recognition.continuous = true;
 	recognition.interimResults = true;
 
-	// Add event listener for when speech recognition starts
+
 	document.querySelector('#start-button').addEventListener('click', () => {
 		recognition.start();
 	});
 
-	// Add event listener for when speech recognition results are available
 	recognition.onresult = (event) => {
 		let result = event.results[event.results.length - 1][0].transcript;
 		document.querySelector('#output').textContent += result;
 	};
+
+	
+	document.querySelector('#stop-button').addEventListener('click', () => {
+		recognition.stop();
+	});
 } else {
 	alert('Web Speech API is not supported in this browser.');
 }
