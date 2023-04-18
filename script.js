cfde59e75f94b102ff567af85fdd34ab438f6e97
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2023-04-18 21:27:24 +0200

    add colors change when the user say the color and reset button

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 if ('webkitSpeechRecognition' in window) {
-
 	let recognition = new webkitSpeechRecognition();
-
 	recognition.continuous = true;
 	recognition.interimResults = true;
-
-
-	document.querySelector('#start-button').addEventListener('click', () => {
-		recognition.start();
-	});
-
-	recognition.onresult = (event) => {
-		let result = event.results[event.results.length - 1][0].transcript;
-		document.querySelector('#output').textContent += result;
+  
+	let colorTable = {
+	  "rose": "pink",
+	  "bleu": "blue",
+	  "vert": "green"
 	};
-
-	
-	document.querySelector('#stop-button').addEventListener('click', () => {
-		recognition.stop();
+  
+	document.querySelector('#start-bouton').addEventListener('click', () => {
+	  recognition.start();
 	});
-} else {
+  
+	recognition.onresult = (event) => {
+	  let result = event.results[event.results.length - 1][0].transcript;
+  
+	  for (let color in colorTable) {
+		if (result.toLowerCase().includes(color)) {
+		  document.body.style.backgroundColor = colorTable[color];
+		  break;
+		} else {
+		  document.body.style.backgroundColor = "";
+		}
+	  }
+  
+	  let output = document.querySelector('#output');
+	  let newParagraph = document.createElement("p");
+	  newParagraph.textContent = result;
+	  output.appendChild(newParagraph);
+	};
+  
+	document.querySelector('#stop-bouton').addEventListener('click', () => {
+	  recognition.stop();
+	});
+  
+	document.querySelector('#reset-bouton').addEventListener('click', () => {
+	  document.body.style.backgroundColor = "";
+	  document.querySelector('#output').innerHTML = '';
+	});
+  } else {
 	alert('Web Speech API is not supported in this browser.');
-}
+  }
+  
